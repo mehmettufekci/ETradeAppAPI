66222d6f44dea5edfdfd47f5430006cc5789ceee
mtufekci1446 <mtufekci1446@gmail.com>
2022-12-24 18:16:22 +0300

    Add project files.

---
+++ Infrastructure/ETradeAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ETradeAPI.Application.Abstractions;
+using ETradeAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPresistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
